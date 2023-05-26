0b7397c574ac8b958ff35975d5e4988c2c6411ff
Eunice Yang <euniceyang21@gmail.com>
2023-05-26 15:11:27 -0500

    Initialize

---
+++ main_app/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+class Plant:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, type, water_needs, sun_needs, alive):
+    self.name = name
+    self.type = type
+    self.water_needs = water_needs
+    self.sun_needs = sun_needs
+    self.alive = alive
+
+plants = [
+  Plant('Aloe', 'succulent', 'low', 'bright', True),
+  Plant('Birds Nest Fern', 'fern', 'moderate', 'moderate', False),
+  Plant('Bromeliad', 'monocot flowering', 'low', 'low', True),
+  Plant('Croton', 'shrub', 'moderate', 'bright', False)
+]
+
+# Define the home view
+def home(request):
+  return HttpResponse('<h1>Hello 🌻🪴🌵</h1>')
+
+def about(request):
+  return render(request, 'about.html')
+
+def plant_index(request):
+  return render(request, 'plants/index.html', {'plants': plants})
