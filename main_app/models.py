bcdec3a03515088f3bb6fd8fa265f3ad73502b4f
Eunice Yang <euniceyang21@gmail.com>
2023-05-28 17:21:15 -0500

    Update model and views

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Plant(models.Model):
+  name = models.CharField(max_length=200)
+  type = models.CharField(max_length=100)
+  water_needs = models.CharField(max_length=100)
+  sun_needs = models.CharField(max_length=100)
+  alive = models.BooleanField(default=True)
+
+  def __str__(self):
+    return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,19 +1,20 @@
 from django.shortcuts import render
+from .models import Plant
 
-class Plant:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, type, water_needs, sun_needs, alive):
-    self.name = name
-    self.type = type
-    self.water_needs = water_needs
-    self.sun_needs = sun_needs
-    self.alive = alive
+# class Plant:  # Note that parens are optional if not inheriting from another class
+#   def __init__(self, name, type, water_needs, sun_needs, alive):
+#     self.name = name
+#     self.type = type
+#     self.water_needs = water_needs
+#     self.sun_needs = sun_needs
+#     self.alive = alive
 
-plants = [
-  Plant('Aloe', 'succulent', 'low', 'bright', True),
-  Plant('Birds Nest Fern', 'fern', 'moderate', 'moderate', False),
-  Plant('Bromeliad', 'monocot flowering', 'low', 'low', True),
-  Plant('Croton', 'shrub', 'moderate', 'bright', False)
-]
+# plants = [
+#   Plant('Aloe', 'succulent', 'low', 'bright', True),
+#   Plant('Birds Nest Fern', 'fern', 'moderate', 'moderate', False),
+#   Plant('Bromeliad', 'monocot flowering', 'low', 'low', True),
+#   Plant('Croton', 'shrub', 'moderate', 'bright', False)
+# ]
 
 # Define the home view
 def home(request):
@@ -23,4 +24,5 @@ def about(request):
   return render(request, 'about.html')
 
 def plant_index(request):
+  plants = Plant.objects.all()
   return render(request, 'plants/index.html', {'plants': plants})
