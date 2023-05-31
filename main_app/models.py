61e5253c693bca82816ab27d96a19eaae0580dd5
Eunice Yang <euniceyang21@gmail.com>
2023-05-31 11:08:19 -0500

    Fix bug in custom water model method

--- main_app/models.py
+++ main_app/models.py
@@ -36,24 +36,16 @@ class Plant(models.Model):
   def get_absolute_url(self):
     return reverse("plant-detail", kwargs={"plant_id": self.id})
   
-  def watered(self):
-    if self.water_needs == 'L':
-      days = 14
-    elif self.water_needs == 'M':
-      days = 7
-    else:
-      days = 2
-    today_date= date.today()
-    adjusted_date = today_date - timedelta(days = days)
-    return adjusted_date
-    # return self.watering_set.filter(date=date.today(): adjusted_date <= date <= today_date).count()
+  def watered_today(self):
+    return self.watering_set.filter(date=date.today()).count() >= 1
 
 class Watering(models.Model):
   date = models.DateField('Watering date')
   plant = models.ForeignKey(Plant, on_delete=models.CASCADE)
 
   def __str__(self):
-    return f"{self.plant} - {self.date}"
+    # return f"{self.plant} - {self.date}"
+    return self.date
   
   # change the default sort
   class Meta:
